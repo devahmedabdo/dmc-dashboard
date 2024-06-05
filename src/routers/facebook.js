ed985420a5ba8b6412b41063da969903ddb7fe81
Ahmed Abdo <devahmedabdo@gmail.com>
2024-06-05 16:08:45 +0300

    try fix pupeeter 4

--- src/routers/facebook.js
+++ src/routers/facebook.js
@@ -7,12 +7,21 @@ app.get("/post", async (req, res) => {
   try {
     const url = req.query.url;
 
-    // Launch Chromium with chrome-aws-lambda
-    const browser = await chromium.puppeteer.launch({
-      args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      // running on the Vercel platform.
+      chromium = require("chrome-aws-lambda");
+      puppeteer = require("puppeteer-core");
+    } else {
+      // running locally.
+      puppeteer = require("puppeteer");
+    }
+
+    let browser = await puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: true,
+      ignoreHTTPSErrors: true,
     });
 
     const page = await browser.newPage();
@@ -47,3 +56,30 @@ app.get("/post", async (req, res) => {
 });
 
 module.exports = app;
+
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  // running locally.
+  puppeteer = require("puppeteer");
+}
+
+const getData = async (url) => {
+  try {
+    let browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
